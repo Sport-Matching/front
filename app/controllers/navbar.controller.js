6f1107c7c2d21e70d99d1f93f65ab5f503d8824f
Cormery Antoine <antoine.cormery@epita.fr>
2016-02-29 02:31:01 +0100

    add md-autocompletion example

--- app/controllers/navbar.controller.js
+++ app/controllers/navbar.controller.js
@@ -12,8 +12,19 @@ text: ""
 };
 }]);*/
 
-angular.module('app').controller('NavBarController', ['$scope', '$state', function($scope, $state) {
-    $scope.search = {
+angular.module('app').controller('NavBarController', ['$scope', '$state', '$log', function($scope, $state, $log) {
+    /*$scope.search = {
         text: ""
-    };
+    };*/
+
+    $scope.querySearch = function(search) {
+        var tab = ["nadal vs federer", "nadal vs tsonga", "murray vs nadal", "gasquet vs tsonga", "gabashvili sv tsonga", "ferrer", "nadal", "federer", "tsonga", "isner", "gasquet", "gabashvili", "roland garros", "wimbledon", "geneva", "gstaad"];
+        var customFilter = function(q) {
+            var lower = angular.lowercase(q);
+            return function(t) {
+                return (t.indexOf(lower) >= 0);
+            }
+        }
+        return search ? tab.filter( customFilter(search) ) : tab;
+    }
 }]);
