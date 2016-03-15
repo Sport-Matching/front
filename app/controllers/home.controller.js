230fa9cde69c9102bc8ef00e334e70bf4da29dd3
Cormery Antoine <antoine.cormery@epita.fr>
2016-03-15 17:00:35 +0100

    Some fix on linear progress

--- app/controllers/home.controller.js
+++ app/controllers/home.controller.js
@@ -1,6 +1,32 @@
 angular.module('app')
-    .controller('HomeController', ['$scope', '$mdBottomSheet','$mdSidenav', '$mdDialog',
+    .controller('HomeController', ['$scope', '$mdBottomSheet', '$mdSidenav', '$mdDialog',
         function($scope, $mdBottomSheet, $mdSidenav, $mdDialog){
-            $scope.showPlayerPart = true;
-            $scope.showPlayerStatPart = true;
+            function generatePlayerView() {
+                $scope.showPlayerPart = true;
+                $scope.showPlayerStatPart = true;
+
+                radialProgress(document.getElementById('player-stat-content-body-radial-main'))
+                        .label("MATCH WIN")
+                        .diameter(180)
+                        .value(25)
+                        .render();
+
+                progressLine(document.getElementById('player-stat-content-body-linear-cnt'))
+                    .valueLeft(33);
+            }
+
+            $scope.clickHardCourtsButton = function() {
+                console.log("clickHardCourtsButton()");
+                generatePlayerView();
+            };
+            $scope.clickClayCourtsButton = function() {
+                console.log("clickClayCourtsButton()");
+                generatePlayerView();
+            };
+            $scope.clickGrassCourtsButton = function() {
+                console.log("clickGrassCourtsButton()");
+                generatePlayerView();
+            };
+
+            generatePlayerView();
     }]);
