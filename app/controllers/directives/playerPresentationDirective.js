a6b53ecd07c7645d7d2f22eed743697806d3e7ac
Antoine Cormery <antoine.cormery@3ie.fr>
2016-03-28 00:18:39 +0200

    Improve presentation part

--- app/controllers/directives/playerPresentationDirective.js
+++ app/controllers/directives/playerPresentationDirective.js
@@ -5,6 +5,7 @@ angular.module('app')
     transclude: true,
     scope: {},
     link: function(scope, element, attrs) {
+        scope.player = {img: "./img/nadal.jpg", name: "Rafael NADAL", nation: "ES", age: 29, birthday: "3 June 1986", weight: "1.85m", favorySurface: "Clay"};
     },
     templateUrl: 'views/directives/playerPresentationDirective.html'
   };
