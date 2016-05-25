b008f53483a529d36e91f2bb9427fe453b4da5a8
Antoine Cormery <antoine.cormery@3ie.fr>
2016-05-25 11:04:24 +0200

    Add pagination on history part

--- app/controllers/directives/historicDirective.js
+++ app/controllers/directives/historicDirective.js
@@ -6,16 +6,37 @@ angular.module('app')
         transclude: true,
         scope: {},
         link: function(scope, element, attrs) {
+            scope.tmpTabHistoric = [];
+            scope.tabHistoric = [];
+            scope.contentOnHistoryPart = scope.tabHistoric.length > 0;
+            scope.haveMoreContent = scope.tmpTabHistoric.length > 0;
+
             scope.$watch(function() {
                 return element.attr('hist');
             }, function(newValue) {
-                var tabHistoric = [];
                 if (attrs.hist !== undefined && attrs.hist != "") {
-                    tabHistoric = JSON.parse(attrs.hist);
+                    scope.tmpTabHistoric = JSON.parse(attrs.hist);
                 }
-                scope.contentOnHistoryPart = tabHistoric.length !== 0;
-                scope.tabHistoric = tabHistoric;
+                scope.tabHistoric = [];
+                var i = 0;
+                while (i < 5 && scope.tmpTabHistoric.length > 0) {
+                    scope.tabHistoric.push(scope.tmpTabHistoric[0]);
+                    scope.tmpTabHistoric.splice(0, 1);
+                    i += 1;
+                }
+                scope.contentOnHistoryPart = scope.tabHistoric.length > 0;
+                scope.haveMoreContent = scope.tmpTabHistoric.length > 0;
             });
+
+            scope.showMoreButtonAction = function() {
+                var i = 0;
+                while (i < 5 && scope.tmpTabHistoric.length > 0) {
+                    scope.tabHistoric.push(scope.tmpTabHistoric[0]);
+                    scope.tmpTabHistoric.splice(0, 1);
+                    i += 1;
+                }
+                scope.haveMoreContent = scope.tmpTabHistoric.length > 0;
+            };
         },
         templateUrl: 'views/directives/historicDirective.html'
     };
